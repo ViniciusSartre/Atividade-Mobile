aaca50c04f59ab5dff7d10a4338bea5983756884
Vinicius Sartre <vinicius_sartre@hotmail.com>
2023-09-14 15:11:43 -0300

    adding service-worker

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+var cacheName = 'Rockband';
+
+self.addEventListener('install', event => {
+
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll([
+
+        './index.html',
+
+        './css/styles.css',
+
+        './js/scripts.js',
+
+        
+      ]))
+  );
+});
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('fetch', function (event) {
+  //Atualizacao internet
+  event.respondWith(async function () {
+    try {
+      return await fetch(event.request);
+    } catch (err) {
+      return caches.match(event.request);
+    }
+  }());
+
+  //Atualizacao cache
+  /*event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );*/
+
+});
